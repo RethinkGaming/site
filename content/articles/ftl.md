e84228ac0a38fb8ae427b34c45103629a6a29d15
Rex Fernando <rex@cs.wisc.edu>
2014-07-22 17:43:11 -0500

    A bunch of stuff, stars work now.

---
+++ content/articles/ftl.md
@@ -0,0 +1,22 @@
+---
+title: "Faster than Light"
+rating: 2.5
+overview: "Put a summary here."
+created_at: "6/19/2014"
+---
+
+Test test test.
+
+# Header
+
+## Header
+
+### Header
+
+#### Header
+
+##### Header
+
+1. list
+2. list
+3. list
---
+++ lib/helpers/star_helper.rb
@@ -0,0 +1,17 @@
+module StarHelper
+  def make_stars(num)
+    sh = ""
+    for i in 1..num
+      sh += "<i class='fa fa-star'></i>"
+    end
+    if num.modulo(1) == 0.5 then
+      sh += "<i class='fa fa-star-half-full'></i>"
+    elsif num.modulo(1) != 0 then
+      abort("Can't have a star value that is not in increments of 0.5.")
+    end
+    if num < 0 or num > 4 then
+      abort("Can't have a star value greater than 4 or less than 0.");
+    end
+    sh
+  end
+end
