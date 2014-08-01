ad03e0a8db16e7798394896260fcf63b5c24d2a2
Rex Fernando <rex@cs.wisc.edu>
2014-08-01 00:07:49 -0500

    fixing ship.sh

--- ship.sh
+++ ship.sh
@@ -5,6 +5,8 @@ CMESSAGE=$(git log --pretty=%B -1)
 rm -rf output
 bundle exec nanoc
 cd output
+git config --global user.email qtreadzsd@gmail.com
+git config --global user.name John Yang
 git init
 git remote add origin git@github.com:RethinkGaming/rethinkgaming.github.io
 git add .
